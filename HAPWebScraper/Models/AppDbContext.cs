55e380a9f1bf6608e7006a04964343564be8dfb6
vjpal3 <vrishalipal@yahoo.com>
2019-11-30 21:15:41 -0500

    Create model and configure DbContext

---
+++ HAPWebScraper/Models/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HAPWebScraper.Models
+{
+    class AppDbContext : DbContext
+    {
+        private const string connectionString = "Server=(localdb)\\ProjectsV13;Database=HAPScraperDB;Trusted_Connection=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<StockData> StocksData { get; set; }
+    }
+}
---
+++ HAPWebScraper/Models/StockData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAPWebScraper.Models
+{
+    class StockData
+    {
+        public int Id { get; set; }
+        public DateTime ScrapeDate { get; set; }
+        public string Company { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? Change { get; set; }
+        public decimal? PercentChange { get; set; }
+        public decimal? PE { get; set; }
+        public decimal? Volume { get; set; }
+        public decimal? YTDChange { get; set; }
+    }
+}
